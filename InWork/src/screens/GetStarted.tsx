f19edd346103013c8f82a359545de211b6caa8a1
Astitva877 <ankurpatle18@gmail.com>
2022-09-04 18:56:08 +0530

    added image in getstarted screen

--- InWork/src/screens/GetStarted.tsx
+++ InWork/src/screens/GetStarted.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, Image, StyleSheet} from 'react-native';
 
 const GetStarted = ({navigation}) => {
   return (
     <View>
-      <Text> Hii </Text>
+      {/* <Text> Hii </Text>
       <Button
         onPress={() => navigation.navigate('LoginScreen')}
         title="Learn More"
         color="#841584"
+      /> */}
+      <Image
+        style={styles.container}
+        source={require('../assests/splash.png')}
+        resizeMode="cover"
       />
-      {/* <Image
-         so
-      />  */}
     </View>
   );
 };
 export default GetStarted;
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    flex: 1,
+  },
+});
